37ce2a4a53a4ca9468eb630ba8a43c94f5a72e10
ErickG233 <1529896768@qq.com>
2021-01-23 12:30:43 +0800

    cleanup: fs: ext4: Remove LGE codes

--- fs/ext4/page-io.c
+++ fs/ext4/page-io.c
@@ -341,7 +341,6 @@ void ext4_io_submit(struct ext4_io_submit *io)
 	if (bio) {
 		int io_op_flags = io->io_wbc->sync_mode == WB_SYNC_ALL ?
 				  WRITE_SYNC : 0;
-
 		if (io->io_flags & EXT4_IO_ENCRYPTED)
 			io_op_flags |= REQ_NOENCRYPT;
 		bio_set_op_attrs(io->io_bio, REQ_OP_WRITE, io_op_flags);
